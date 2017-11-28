98f510ad963c2243db6591cb4683f7c00ecda822
zac6ix <nanaquameallday@gmail.com>
2017-11-28 21:33:15 +0000

    OTA Assert update

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -80,7 +80,7 @@ BOARD_FLASH_BLOCK_SIZE := 131072
 TARGET_KMODULES := true
 
 # Assert
-TARGET_OTA_ASSERT_DEVICE := Moto_E4,woods,woods_retail,e4
+TARGET_OTA_ASSERT_DEVICE := Moto_E4,Moto E4,moto_e4,e4,woods,woods_f
 
 # Disable memcpy opt (for audio libraries)
 TARGET_CPU_MEMCPY_OPT_DISABLE := true
