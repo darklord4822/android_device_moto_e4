fdbf73a1365408f697cdd9c0c4572d8b9ac4540e
darklord4822 <darklord4722@gmail.com>
2018-02-18 02:23:13 +0000

    full fix light sensor

--- device_e4.mk
+++ device_e4.mk
@@ -168,10 +168,6 @@ PRODUCT_PACKAGES += \
 # Keymaster HAL
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl
-
-# Lights
-PRODUCT_PACKAGES += \
-    lights.mt6737m
     
 # Sensors HAL
 PRODUCT_PACKAGES += \
@@ -179,10 +175,7 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service \
     android.hardware.sensors@1.0-impl \
     android.hardware.sensors@1.0-service \
-   
-# Drm HAL 
-PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl
+    lights.mt6737m
     
 # GPS HAL
 PRODUCT_PACKAGES += \
@@ -211,7 +204,6 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.mapper@2.0-impl \
-    android.hardware.renderscript@1.0-impl \
     android.hardware.memtrack@1.0-impl \
     libgralloc_extra \
     libgui_ext \
@@ -231,10 +223,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
     media.stagefright.legacyencoder=0
- 
-# RIL
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.kernel.android.checkjni=0
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 	persist.sys.usb.config=mtp
